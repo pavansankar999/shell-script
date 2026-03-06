10dd4496c611a37f1af5dbe4b02c3b25cfcaeb15
pavan <info@pavan.seela.com>
2026-03-06 21:54:50 +0530

    conversation

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,3 +3,4 @@
 echo "plesase enter your username ::" #here we are asking user to enter username and then we will store that in variable called USERNAME
 read -s  USERNAME #here username is variable and read is used to take input from user and store in variable
 echo "usename entered: $USERNAME"
+echo "please enter your password ::" #here we are asking user to enter password and then we will store that in variable called PASSWORD
