2e898b7016b23339a41a3ecceb23dd7e771b7529
pavan <info@pavan.seela.com>
2026-03-06 21:50:04 +0530

    conversation

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "plesase enter your username ::" #here we are asking user to enter username and then we will store that in variable called USERNAME
-read USERNAME #here username is variable and read is used to take input from user and store in variable
+read -s  USERNAME #here username is variable and read is used to take input from user and store in variable
 echo "usename entered: $USERNAME"
