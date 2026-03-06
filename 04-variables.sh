c6339a573103129e1b2a9de0a759e398b0ea5583
pavan <info@pavan.seela.com>
2026-03-06 21:33:39 +0530

    conversation

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1 #no pace before and after = sign
+PERSON2=$2 #no pace before and after = sign
+
+echo "$PERSON1:hi $PERSON2"
+echo "$PERSON2:i am good  how are you"
+echo "$PERSON1: i am good too"
