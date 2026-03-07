0f14aa32643e0d34c0e348c61acf6bdae33aa761
pavan <info@pavan.seela.com>
2026-03-07 15:00:33 +0530

    nw conversation new data type is adding

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,6 +5,16 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run the root user "
+    exit 1 #other than 0
 fi
 
-sudo dnf install mysql y
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "my sql installtion is successful"
+    exit 1
+else
+     echo "my sql installtion is successful"
+fi
+dnf install mysql -y
