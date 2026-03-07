788f812cf019ac72f8fe446ea3775c3317236b06
pavan <info@pavan.seela.com>
2026-03-07 15:12:05 +0530

    nw conversation new data type is adding

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -4,27 +4,28 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "please run the root user "
-    exit 1 #other than 0
+    echo "please run with root user"
+    exit 1 
 fi
 
-dnf install mysql -y
+# Installing MySQL (using mysql-server for AWS Linux)
+dnf install mysql-server -y
 
 if [ $? -ne 0 ]
 then 
-    echo "my sql installtion is successful"
+    echo "my sql installation failed"
     exit 1
 else
-     echo "my sql installtion is successful"
+     echo "my sql installation is successful"
 fi
 
-
+# Installing Git
 dnf install git -y
 
 if [ $? -ne 0 ]
 then 
-    echo "my git installtion is successful"
+    echo "my git installation failed"
     exit 1
 else
-     echo "my git installtion is successful"
+     echo "my git installation is successful"
 fi
