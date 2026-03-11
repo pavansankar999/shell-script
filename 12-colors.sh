b719a65902b332baa3c610cc62368de8efdfd080
pavan <info@pavan.seela.com>
2026-03-11 23:21:14 +0530

    ne code added

---
+++ 12-colors.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="E[35M"]
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run with root user"
+    exit 1 
+fi
+
+# Changed 'mysql-server' to 'mariadb105-server'
+dnf install mariadb105-server -y
+
+if [ $? -ne 0 ]
+then 
+    echo "my sql installation failed"
+    exit 1
+else
+     echo "my sql installation is successful"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "my git installation failed"
+    exit 1
+else
+     echo "my git installation is successful"
+fi
