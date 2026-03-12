08aa3ae6fda83da4dac7373de9afacd81ed46370
pavan <info@pavan.seela.com>
2026-03-12 23:16:56 +0530

    adding new second line

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
 USERID=$(id -u)
-# Fixed color codes (R was slightly off, added N for Reset)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-# Ensure the log folder exists
-mkdir -p $LOGS_FOLDER
-
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-# Function to validate previous command status
 VALIDATE(){
-    if [ $1 -ne 0 ]; then 
-        echo -e "$2 ... $R FAILED $N"
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-# Check if user is root
-if [ $USERID -ne 0 ]; then
-    echo "ERROR: Please run this script with root access"
-    exit 1
-fi
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-# Loop through all packages passed as arguments
-for package in "$@"
+CHECK_ROOT
+
+for package in $@
 do
     dnf list installed $package &>>$LOG_FILE_NAME
-    
-    if [ $? -ne 0 ]; then
+    if [ $? -ne 0 ]
+    then
         dnf install $package -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing $package"
     else
