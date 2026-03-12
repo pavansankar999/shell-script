18fef4891fd52fb54107c385a73de883d57fd20a
pavan <info@pavan.seela.com>
2026-03-12 23:10:09 +0530

    adding new second line

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="\e[35M"
-G="\e[32M"
-Y="\e[33M"
+# Fixed color codes (R was slightly off, added N for Reset)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 LOGS_FOLDER="/var/log/shellscript-logs"
+# Ensure the log folder exists
+mkdir -p $LOGS_FOLDER
+
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
+# Function to validate previous command status
 VALIDATE(){
-
-
-
-if [ $? -ne 0 ]
-then 
-    echo "my sql installation failed"
-    exit 1
-else
-     echo "my sql installation is successful"
-fi
+    if [ $1 -ne 0 ]; then 
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
-echo "script started executing at : $TIMESTAMP" &>>&LOG_FILE_NAME
-
-dnf install git -y
-
-if [ $? -ne 0 ]
-then 
-    echo "my git installation failed"
+# Check if user is root
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run this script with root access"
     exit 1
-else
-     echo "my git installation is successful"
 fi
 
-for package in $@
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+# Loop through all packages passed as arguments
+for package in "$@"
 do
     dnf list installed $package &>>$LOG_FILE_NAME
-    if[$? -ne 0]
-    then
+    
+    if [ $? -ne 0 ]; then
         dnf install $package -y &>>$LOG_FILE_NAME
-        VALIDATE $? "installing $package"
-        else
-            echo -e "$package is already $Y ... INSTALLED $N"
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already $Y ... INSTALLED $N"
     fi
-
 done
